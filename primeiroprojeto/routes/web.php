b961affd79185f735273de459630f16a9119f1de
php 33 <php.33@metralha.cec.ime.usp.br>
2017-02-10 21:17:03 -0200

    acrescentando arquivos saudacao

--- primeiroprojeto/routes/web.php
+++ primeiroprojeto/routes/web.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/saudacao', function(){
+ return view('saudacao');
 });
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
